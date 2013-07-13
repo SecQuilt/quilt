5b52895f6b375028e9e0e7cb6e876abe5d18cbb5
Derrick Karimi <dhkarimi@sei.cmu.edu>
2013-07-12 22:23:21 -0400

    removed unused function, merged master

--- lib/quilt_interpret.py
+++ lib/quilt_interpret.py
@@ -525,13 +525,6 @@ def until(before, after):
     #   newly determined events
     return _pattern(returnEventsId, returnEvents)
 
-def quand (*patterns):
-    """
-
-    :param patterns:
-    """
-    pass
-
 def _get_events(eventsId):
 #   logging.debug("accessing " + str(eventsId))
     return globals()['_eventPool'][eventsId]
