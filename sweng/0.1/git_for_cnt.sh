30c9a8ebbbf26b9fb973cbbca0d3e35b82dda31a
Derrick Karimi <dhkarimi@sei.cmu.edu>
2013-08-24 00:04:11 -0400

    script for repeatable counting

---
+++ sweng/0.1/git_for_cnt.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+tag=$1
+
+if [ -z "$tag" ] ; then
+    echo "Tag required"
+    exit -1
+fi
+
+echo "Getting quilt ($tag) for counting purposes"
+
+mkdir -p cnt
+cd cnt
+rm -rf quilt quilt_${tag}
+git clone git@romano:quilt
+mv quilt quilt_${tag}
+cd quilt_${tag}
+git checkout $tag
+
+echo Removing soft links
+find . -type l -exec rm -f {} \;
+echo Removing sweng files
+rm -rf sweng
+echo Removing text files
+find . -name "*.txt" -exec rm -f {} \;
+echo Removing test data files
+rm -rf test/var/log
+echo "Performing raw line count"
+wc -l $(find . -type f) | tail -n 1
+
