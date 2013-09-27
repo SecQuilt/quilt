3332835cc09329576706551d82c0c34b8a538868
Derrick Karimi <dhkarimi@sei.cmu.edu>
2013-09-27 11:10:25 -0400

    now sporting bsd license

--- lib/quilt_define.py
+++ lib/quilt_define.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python
-# Copyright 2013 Carnegie Mellon University 
+# Copyright (c) 2013 Carnegie Mellon University.
+# All Rights Reserved.
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, 
+# this list of conditions and the following acknowledgments and disclaimers.
+# 2. Redistributions in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following acknowledgments and disclaimers in 
+# the documentation and/or other materials provided with the distribution.
+# 3. Products derived from this software may not include "Carnegie Mellon 
+# University," "SEI" and/or "Software Engineering Institute" in the name of 
+# such derived product, nor shall "Carnegie Mellon University," "SEI" and/or 
+# "Software Engineering Institute" be used to endorse or promote products 
+# derived from this software without prior written permission. For written 
+# permission, please contact permission@sei.cmu.edu.
+# Acknowledgments and disclaimers:
+# This material is based upon work funded and supported by the Department of 
+# Defense under Contract No. FA8721-05-C-0003 with Carnegie Mellon University 
+# for the operation of the Software Engineering Institute, a federally funded 
+# research and development center. 
 #  
-# This material is based upon work funded and supported by the Department of Defense under Contract No. FA8721-
-# 05-C-0003 with Carnegie Mellon University for the operation of the Software Engineering Institute, a federally 
-# funded research and development center. 
+# Any opinions, findings and conclusions or recommendations expressed in this 
+# material are those of the author(s) and do not necessarily reflect the views 
+# of the United States Department of Defense. 
 #  
-# Any opinions, findings and conclusions or recommendations expressed in this material are those of the author(s) and 
-# do not necessarily reflect the views of the United States Department of Defense. 
+# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING 
+# INSTITUTE MATERIAL IS FURNISHEDON AN "AS-IS" BASIS.  CARNEGIE MELLON 
+# UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS 
+# TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE 
+# OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE 
+# MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND 
+# WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT. 
 #  
-# NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING INSTITUTE 
-# MATERIAL IS FURNISHEDON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO 
-# WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, 
-# BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE OR MERCHANTABILITY, 
-# EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON 
-# UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM 
-# PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT. 
+# This material has been approved for public release and unlimited distribution.
+#
+# Carnegie Mellon(r), CERT(r) and CERT Coordination Center(r) are registered 
+# marks of Carnegie Mellon University. 
 #  
-# This material has been approved for public release and unlimited distribution except as restricted below. 
-#  
-# Internal use:* Permission to reproduce this material and to prepare derivative works from this material for internal 
-# use is granted, provided the copyright and "No Warranty" statements are included with all reproductions and 
-# derivative works. 
-#  
-# External use:* This material may be reproduced in its entirety, without modification, and freely distributed in 
-# written or electronic form without requesting formal permission. Permission is required for any other external and/or 
-# commercial use. Requests for permission should be directed to the Software Engineering Institute at 
-# permission@sei.cmu.edu. 
-#  
-# * These restrictions do not apply to U.S. government entities. 
-#  
-# Carnegie Mellon(r), CERT(r) and CERT Coordination Center(r) are registered marks of Carnegie Mellon University. 
-#  
-# DM-0000632 
-
+# DM-0000632
 import sys
 import quilt_core
 import quilt_data
